cbdc9086dc37e003a467079187a21c83b627f675
Sora-210 <sokominto1231@gmail.com>
2021-10-18 12:30:49 +0000

    [update] setting of hotreload

--- group-todo/vue.config.js
+++ group-todo/vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  configureWebpack: {
+    devServer: {
+      proxy: 'http://localhost:8080',
+      watchOptions: {
+        ignored: /node_modules/,
+        poll: true
+      }
+    }
+  }
 }
