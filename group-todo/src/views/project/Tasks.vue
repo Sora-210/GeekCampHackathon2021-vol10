6066be2f76b845764436e4307d95499622b97d10
Sora-210 <sokominto1231@gmail.com>
2021-10-19 12:01:35 +0000

    [create] projectViews

---
+++ group-todo/src/views/project/Tasks.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="all">
+        <project-nav>
+        </project-nav>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import ProjectNav from '../../components/ProjectNav.vue'
+
+export default Vue.extend({
+    name: 'ProjectTasks',
+    components: {
+        ProjectNav
+    }
+})
+</script>
+
+<style scoped>
+#all {
+    height: 100%;
+}
+</style>
