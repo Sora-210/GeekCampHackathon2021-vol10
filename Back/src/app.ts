e7abc95048c645b945b0c7ccb1f90b1b352de086
Sora-210 <sokominto1231@gmail.com>
2021-10-24 03:57:08 +0900

    [create] project api

--- Back/src/app.ts
+++ Back/src/app.ts
@@ -4,22 +4,34 @@ import { json as BodyParserJson } from 'body-parser'
 import cors from 'cors'
 import { initializeApp } from 'firebase-admin/app'
 import { getAuth, Auth } from 'firebase-admin/auth'
-
+import { dbInstance } from './db/instance'
+//###################################################
+//DB access check
+dbInstance.authenticate()
+    .then((r) => {
+        console.log("DB success")
+    })
+    .catch((e) => {
+        console.log("DB error")
+        console.log(e)
+    })
 
 //###################################################
 //MagicNumber
 const PORT:Number = 8000
 //###################################################
+//express setting
 const app:express.Express = express()
 app.use(BodyParserJson())
 app.use(cors())
 
+//firebase setting
 initializeApp({
     projectId: 'hackathon-gorupdev-tool'
 })
 const auth:Auth = getAuth()
 //###################################################
-//tokenCheck
+//jwtCheck
 app.all('*', (req, res, next) => {
     const token:string = req.header('Authorization')!
     auth.verifyIdToken(token)
@@ -38,4 +50,5 @@ app.use('/projects', projectsRouter)
 //###################################################
 app.listen(PORT, () => {
     console.log("API Server Started.")
+    console.log("Listening port:" + PORT)
 })
